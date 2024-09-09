c7d90383a69fe173bfed3d893fc792f0747d48b1
Mahnegar Mahmoudi <mahnegar.mahmoudi@sekke.local>
2024-09-09 13:54:16 +0330

    .

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -11,6 +11,7 @@ const Section = ({
 }) => {
   return (
     <div
+      id={id}
       className={`relative ${
         CustopmPaddings ||
         `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""} ${
